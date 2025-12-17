c984f5d645be5cf14bb9f600bb4f6f314614c59c
hotluner <dimkoval1@gmail.com>
2025-12-18 00:44:26 +0700

    5. add PointerInputDrag

--- app/src/main/java/com/example/gesturedemo/MainActivity.kt
+++ app/src/main/java/com/example/gesturedemo/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.Orientation
+import androidx.compose.foundation.gestures.detectDragGestures
 import androidx.compose.foundation.gestures.detectTapGestures
 import androidx.compose.foundation.gestures.draggable
 import androidx.compose.foundation.gestures.rememberDraggableState
@@ -52,6 +53,7 @@ fun MainScreen(modifier: Modifier = Modifier) {
     ClickDemo(modifier)
     TapPressDemo(modifier)
     DragDemo(modifier)
+    PointerInputDrag(modifier)
 }
 
 @Composable
@@ -124,8 +126,25 @@ fun DragDemo(modifier: Modifier = Modifier) {
     }
 }
 
-
-
+@Composable
+fun PointerInputDrag(modifier: Modifier = Modifier) {
+    Box(modifier = modifier.fillMaxSize()) {
+        var xOffset by remember { mutableStateOf(0f) }
+        var yOffset by remember { mutableStateOf(0f) }
+        Box(
+            Modifier
+                .offset { IntOffset(xOffset.roundToInt(), yOffset.roundToInt()) }
+                .background(Color.Blue)
+                .size(100.dp)
+                .pointerInput(Unit) {
+                    detectDragGestures { _, distance ->
+                        xOffset += distance.x
+                        yOffset += distance.y
+                    }
+                }
+        )
+    }
+}
 
 @Preview(showBackground = true)
 @Composable
